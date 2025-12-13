bb244f1c6b6bb4f4fb51903dee6f41091e1a7a00
root <root@vmgit.5odh1l40srlebhuuxlnmj2xiwa.ix.internal.cloudapp.net>
2025-12-13 07:53:59 +0000

    Revert "from prod server"
    This reverts commit c875d2a8c320efdc8a82a4e05de02ff52a6fba25.

--- app/Http/Requests/ContactMailRequest.php
+++ app/Http/Requests/ContactMailRequest.php
@@ -3,7 +3,7 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use App\Rules\ReCaptchaV3;
+use App\Rules\RecaptchaV3;
 
 class ContactMailRequest extends FormRequest
 {
