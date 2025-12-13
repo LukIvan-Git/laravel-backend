3b98220980965345cb1b8aa731bee8e155134238
Ivan <ivanluk78@gmail.com>
2025-12-13 16:07:37 +0800

    fix recaptcha login

--- resources/views/contact_me.blade.php
+++ resources/views/contact_me.blade.php
@@ -64,15 +64,15 @@
         </div>
 </main>
 <script>
-    grecaptcha.enterprise.ready(function() {
-        document.getElementById('contactform').addEventListener("submit", function(event) {
+    document.getElementById('contactform').on("submit", function(event) {
             event.preventDefault();
             var form = this;
-            grecaptcha.enterprise.execute('{{ env("GOOGLE_RECAPTCHA_SITE_KEY") }}', { action: 'contactme' }).then(function(token) {
+            grecaptcha.enterprise.ready(function() {
+                grecaptcha.enterprise.execute('{{ env("GOOGLE_RECAPTCHA_SITE_KEY") }}', { action: 'contactme' }).then(function(token) {
                 document.getElementById('contact-me-recaptcha-token').value = token;
                 form.submit();
+                });
             });
-            }, false);
     });
 </script>
 @endsection
